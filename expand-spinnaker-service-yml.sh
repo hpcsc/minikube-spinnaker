7cbbcf45160ddd9da9db00d1ac87517db5d3b211
David Nguyen <hpcsc@users.noreply.github.com>
2021-08-09 23:15:03 +1000

    Add orca profile if exists

--- expand-spinnaker-service-yml.sh
+++ expand-spinnaker-service-yml.sh
@@ -2,14 +2,25 @@
 
 set -euo pipefail
 
+function get_escaped_yaml() {
+    local file_path=$1
+    if [ -f "${file_path}" ]; then
+        echo "\"$(sed 's/\"/\\"/g' "${file_path}")\""
+    else
+        echo "{}"
+    fi
+}
+
 KUBE_CONFIG=$(./export-kube-config.sh)
 MINIKUBE_IP=$(minikube ip)
 DECK_NODEPORT=32070
 GATE_NODEPORT=32064
+ORCA_PROFILE="$(get_escaped_yaml ./profiles/orca.yml)"
 
 cat ./spinnakerservice.yaml | \
     yq eval ".spec.spinnakerConfig.config.artifacts.gitrepo.accounts[0].token = env(GITHUB_TOKEN) | \
                         .spec.spinnakerConfig.files.local-kubernetes = \"${KUBE_CONFIG}\" | \
+                        .spec.spinnakerConfig.profiles.orca = ${ORCA_PROFILE} | \
                         .spec.spinnakerConfig.service-settings.deck.overrideBaseUrl = \"http://${MINIKUBE_IP}:${DECK_NODEPORT}\" | \
                         .spec.spinnakerConfig.service-settings.deck.env.API_HOST = \"http://${MINIKUBE_IP}:${GATE_NODEPORT}\" | \
                         .spec.spinnakerConfig.service-settings.gate.overrideBaseUrl = \"http://${MINIKUBE_IP}:${GATE_NODEPORT}\"" -
